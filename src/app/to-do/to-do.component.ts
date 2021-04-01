9cda56f10645c15452029dfde2f059c01e23f9aa
Maggie Oudsema <maggieoudsema@gmail.com>
2021-03-31 23:55:01 -0400

    added spacing around the complete button

---
+++ src/app/to-do/to-do.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../todo';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faClipboardCheck, faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+
+@Component({
+  selector: 'app-to-do',
+  templateUrl: './to-do.component.html',
+  styleUrls: ['./to-do.component.css'],
+})
+export class ToDoComponent implements OnInit {
+  faCoffee = faClipboardCheck;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  todo: Todo[] = [
+    { task: 'dishes', completed: false },
+    { task: 'wash the dog', completed: false },
+    { task: 'make the bed', completed: true },
+    { task: 'laundry', completed: true },
+    { task: 'sweep the floor', completed: false },
+  ];
+}
